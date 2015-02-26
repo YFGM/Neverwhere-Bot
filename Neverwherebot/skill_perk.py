914012be2210e5f872734493bd1ad342c818eaca
YFGM <YFGM@users.noreply.github.com>
2015-02-26 14:19:58 +0100

    Feexan boogs

--- Neverwherebot/skill_perk.py
+++ Neverwherebot/skill_perk.py
@@ -10,10 +10,12 @@ class Perk(Perk):
         char = models.Character.objects.get(name=character)
         skill = models.Skill.objects.get(name=self.name)
         if not models.CharacterSkill.objects.filter(skill=skill).filter(character=char).exists:
+            print "Char skill not present, creating."
             new = models.CharacterSkill()
             new.skill = skill
             new.character = char
             new.save()
+            print "Char skill created."
             return True
 
     def on_recalc(self, character):
@@ -21,16 +23,19 @@ class Perk(Perk):
             char = models.Character.objects.get(name=character)
         except:
             print "Character not found."
+            return
 
         try:
             skill = models.Skill.objects.get(name=self.name)
         except:
             print "Skill not found."
+            return
 
         try:
             charskill = models.CharacterSkill.objects.filter(skill=skill).get(character=char)
         except:
             print "Charskill not found."
+            return
         if charskill.level == 0 or charskill.level == 2:
             charskill.level += 2
         else:
